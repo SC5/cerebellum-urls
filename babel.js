5785cf70363b82ba56a02aa821a7f4671007bc39
Lari Hoppula <lari@sc5.io>
2015-08-11 10:50:22 +0300

    Prepare for css-modules rewrite

--- babel.js
+++ babel.js
@@ -1,8 +1,5 @@
-// disable .css requires on server side
-require.extensions['.css'] = function() {
-  return null;
-};
-
 // require babel/register here so we can write the server with ES201X as well
 require('babel/register');
+// load css-modules also on server side
+require('css-modules-require-hook');
 require('./server');
